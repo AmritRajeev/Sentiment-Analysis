badbab03e646d02c18100b636d52e837aa782940
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-11-17 04:07:57 +0530

    Update sentiment_analysis_using_vader.py

--- sentiment_analysis_using_vader.py
+++ sentiment_analysis_using_vader.py
@@ -20,13 +20,26 @@ def sentiment_scores(sentence):
     else : 
         return "neutral"
 
+def acc_check(ct):
+    acc=ct/len(range(0,17))
+    acc_per=acc*100
+    return acc_per
+    
+    
 
-        
+
 # Driver code 
 if __name__ == "__main__" :
 
+    ct=0
     df=pd.read_csv(r"C:\Users\User\Desktop\python prgs\Tweets.csv")
-    for i in range(1,12): 
+    for i in range(0,17): 
                 sentence=df.text[i]
-                sentiment_scores(sentence)
-                print("The sentence is",sentiment_scores(sentence))
+                #df.airline_sentiment[i]=sentiment_scores(sentence)
+                #print(sentiment_scores(sentence),df.text[i])
+                #print(sentiment_scores(sentence),df.airline_sentiment[i])
+                if(sentiment_scores(sentence)==df.airline_sentiment[i]):
+                    ct+=1
+                   
+    acc_check(ct)
+    print("The accuracy of the model is",acc_check(ct))
