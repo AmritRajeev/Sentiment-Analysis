b8c12ac7bb6968c5a18e197d8671183e33366490
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-11-17 07:53:53 +0530

    Update sentiment_analysis_using_vader.py

--- sentiment_analysis_using_vader.py
+++ sentiment_analysis_using_vader.py
@@ -5,6 +5,12 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+import nltk
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+
 
 def sentiment_scores(sentence):  
     sent_obj = SentimentIntensityAnalyzer() 
@@ -24,9 +30,21 @@ def acc_check(ct):
     acc=ct/len(range(0,17))
     acc_per=acc*100
     return acc_per
-    
-    
 
+def get_word_sentiment(text):
+    
+    tokenized_text = nltk.word_tokenize(text)
+    sent_obj = SentimentIntensityAnalyzer() 
+ 
+    for word in tokenized_text:
+        sentiment_dict = sent_obj.polarity_scores(word)
+        if (sentiment_dict['compound']) >= 0.1:
+            pos_word_list.append(word)
+        elif (sentiment_dict['compound']) <= -0.1:
+            neg_word_list.append(word)
+        else:
+            neu_word_list.append(word)
+    
 
 # Driver code 
 if __name__ == "__main__" :
@@ -43,3 +61,17 @@ if __name__ == "__main__" :
                    
     acc_check(ct)
     print("The accuracy of the model is",acc_check(ct))
+    pos_word_list=[]
+    neu_word_list=[]
+    neg_word_list=[] 
+    for i in range(0,17): 
+                text=df.text[i]
+                get_word_sentiment(text)
+    
+    print('Positive:',pos_word_list)        
+    print('Negative:',neg_word_list) 
+    for j in neu_word_list:
+        if (len(j)<=4):
+            neu_word_list.remove(j)
+    neu_word_list.remove('@')        
+    print('Neutral:',neu_word_list)
